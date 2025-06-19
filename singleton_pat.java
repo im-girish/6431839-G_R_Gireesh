fa1186e64d227cc8a01091f0d31b06a93034bc0e
G R Gireesh <girishgiri9789@gmail.com>
2025-06-19 10:46:13 +0530

    Initial commit: Singleton Pattern Example

---
+++ singleton_pat.java
@@ -0,0 +1,29 @@
+public class singleton_pat {
+
+    private static singleton_pat i;
+
+    private singleton_pat() {
+        System.out.println("Singleton I Created");
+    }
+
+    public static singleton_pat getI() {
+        if (i == null) {
+            i = new singleton_pat();
+        }
+        return i;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello from Singleton");
+    }
+
+    public static void main(String[] args) {
+        singleton_pat obj1 = singleton_pat.getI();
+        obj1.showMessage();
+
+        singleton_pat obj2 = singleton_pat.getI();
+        obj2.showMessage();
+
+        System.out.println("Are both objects same? " + (obj1 == obj2));
+    }
+}
